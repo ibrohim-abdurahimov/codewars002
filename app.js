cdf0729d11292db6103e43f0894d6a0bb412ac6a
ibrohim-abdurahimov <ibr20040322@gmail.com>
2024-09-13 07:54:48 +0500

    masala

---
+++ app.js
@@ -0,0 +1,22 @@
+{
+    // console.log("Masala-1")
+    // function repeatStr (n, s) {
+    //     return s.repeat(n)
+    // }
+    // console.log(repeatStr(3,"I"))
+}
+{
+    // console.log("Masala-2")
+    // function removeChar(str){
+    //     return str.slice(1,-1)
+       
+    // }
+    // console.log(removeChar("Hello"))
+}
+{
+    // console.log("Masala-3")
+    // function makeUpperCase(str) {
+    //     return str.toUpperCase()
+    // }
+    // console.log(makeUpperCase("salom"))
+}
